ed4ff101aac64d210130d26de3017aec284abce3
Zaephor <Zaephor@users.noreply.github.com>
2019-08-01 21:13:06 -0700

    test: changed __tests__ folder to test

--- test/ah-auth-plugin.test.js
+++ test/ah-auth-plugin.test.js
@@ -1,32 +1,33 @@
 /* eslint-env node, mocha */
+/* eslint-disable no-unused-expressions */
 const fs = require('fs')
 const path = require('path')
-const {expect} = require('chai')
+const { expect } = require('chai')
 const ActionHero = require('actionhero')
 const actionhero = new ActionHero.Process()
 
 process.env.PROJECT_ROOT = path.join(require.resolve('actionhero'), '..')
-let knexConfig = require(path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..', 'config', 'ah-knex-plugin.js'))
-let knexEnv = (process.env.NODE_ENV && knexConfig[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
-let sessionConfig = require(path.join(require.resolve('@zaephor-ah/ah-session-plugin'), '..', 'config', 'ah-session-plugin.js'))
-let sessionEnv = (process.env.NODE_ENV && sessionConfig[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
+const knexConfig = require(path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..', 'config', 'ah-knex-plugin.js'))
+const knexEnv = (process.env.NODE_ENV && knexConfig[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
+const sessionConfig = require(path.join(require.resolve('@zaephor-ah/ah-session-plugin'), '..', 'config', 'ah-session-plugin.js'))
+const sessionEnv = (process.env.NODE_ENV && sessionConfig[process.env.NODE_ENV]) ? process.env.NODE_ENV : 'default'
 let api
 
 describe('ah-auth-plugin', () => {
-  let configChanges = {
+  const configChanges = {
     'ah-knex-plugin': knexConfig[knexEnv]['ah-knex-plugin'](ActionHero.api),
     'ah-session-plugin': sessionConfig[sessionEnv]['ah-session-plugin'](ActionHero.api),
     // 'ah-objection-plugin': config[environment]['ah-objection-plugin'](ActionHero.api),
     plugins: {
-      'ah-session-plugin': {path: path.join(require.resolve('@zaephor-ah/ah-session-plugin'), '..')},
-      'ah-knex-plugin': {path: path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..')},
-      'ah-objection-plugin': {path: path.join(require.resolve('@zaephor-ah/ah-objection-plugin'), '..')},
-      'ah-auth-plugin': {path: path.join(__dirname, '..')}
+      'ah-session-plugin': { path: path.join(require.resolve('@zaephor-ah/ah-session-plugin'), '..') },
+      'ah-knex-plugin': { path: path.join(require.resolve('@zaephor-ah/ah-knex-plugin'), '..') },
+      'ah-objection-plugin': { path: path.join(require.resolve('@zaephor-ah/ah-objection-plugin'), '..') },
+      'ah-auth-plugin': { path: path.join(__dirname, '..') }
     }
   }
 
   before(async () => {
-    api = await actionhero.start({configChanges})
+    api = await actionhero.start({ configChanges })
   })
 
   after(async () => {
@@ -50,16 +51,16 @@ describe('ah-auth-plugin', () => {
   it('TODO: should validate the user model was loaded')
 
   // Create and retrieve user check directly from model
-  let dummyUser = {email: 'test@domain.com', password: 'password123'}
+  const dummyUser = { email: 'test@domain.com', password: 'password123' }
   it('create user entry', async () => {
-    let newUserObject = await api.models.user.query().insert(dummyUser)
+    const newUserObject = await api.models.user.query().insert(dummyUser)
     expect(newUserObject.email).to.equal(dummyUser.email)
     expect(await newUserObject.verifyPassword(dummyUser.password)).to.equal(true)
     expect(newUserObject.uuid).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/)
     dummyUser.uuid = newUserObject.uuid
   })
   it('obtain user entry', async () => {
-    let userObject = await api.models.user.query().where('email', dummyUser.email).limit(1).first()
+    const userObject = await api.models.user.query().where('email', dummyUser.email).limit(1).first()
     expect(userObject.email).to.equal(dummyUser.email)
     expect(await userObject.verifyPassword(dummyUser.password)).to.equal(true)
     expect(userObject.uuid).to.equal(dummyUser.uuid)
@@ -74,5 +75,4 @@ describe('ah-auth-plugin', () => {
 
   it('TODO: validate that data.auth is false if no session')
   it('TODO: validate that data.auth is contains the user if theres a session')
-
 })
